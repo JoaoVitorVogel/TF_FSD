ca108c92d3a0b29e428e1f7abe8029a2d0534caa
Joao Vitor <joaovitorvogel85@gmail.com>
2023-11-24 07:57:29 -0300

    add

--- PseudoCodigo.java
+++ PseudoCodigo.java
@@ -13,15 +13,19 @@ public class PseudoCodigo {
     static int t5 = 0;
     static int t6 = 0;
     static int t7 = 0;
+
     static int t8 = 0;
     static int t9 = 0;
 
     //Declaração de variaveis na memoria
         
-    static int[] A = {7, 15, 9, 12, 5, -23, -6, -17, -18, -21};
-    static int[] B = {16, 13, 5, 9, 54, -24, -5, -8, -20, -12};
+    static int[] A = {7, 15, 9, 22, 5, -3, -6, -17, -1, -21};
+    static int[] B = {16, 13, 5, 9, 54, -4, -5, -8, -2, -12};
     static int[] C = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
     static int[] D = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    static int mv = 0;  //maior valor dos vetores
+    static int st = 0;  //soma total dos vetores
+    static int sm = 0;  //resultado final da multuplicação
 
 
     public static void main(String[] args){
@@ -35,7 +39,58 @@ public class PseudoCodigo {
             subtrai();
         }
 
+        t4 = 0;
+        for(t1=0; t1 < 10; t1++){
+            maiorValor();
+        }
+        mv = t4;
+
+        t4 = 0;
+        for(t1=0; t1 < 10; t1++){
+            somaTodos();
+        }
+        st = t4;
+
         
+        t2 = st; //resultado da soma
+        t3 = mv; //maior valor, ou total de repetições
+        t4 = 0; 
+
+        for(t1=0; t1 < t3; t1++){
+            multiplica();
+        }
+        
+        sm = t4;
+
+    
+    }
+
+    public static void multiplica(){
+        
+        t4 = t4 + t2;
+    }
+
+    public static void somaTodos(){
+
+        t2 = C[t1];
+        t3 = D[t1];
+
+        t4 = t4 + t2;
+        t4 = t4 + t3;
+
+    }
+
+    public static void maiorValor(){
+        
+        t2 = C[t1];
+        t3 = D[t1];
+
+        if(t4 < t2){
+            t4 = t2;
+        }
+        if(t4 < t3){
+            t4 = t3;
+        }
     }
 
     public static void soma(){
@@ -58,27 +113,4 @@ public class PseudoCodigo {
 
     }
     
-
-    public static int multiplica(int a, int b){
-        int acumula = 0;
-        int vezes = b;
-
-        // se B eh negativo, troca o sinal
-        if(b < 0){
-            vezes = 0 - vezes;
-        }
-        
-        // acumula A, B vezes
-        while(vezes!=0){
-            acumula = acumula + a;
-            vezes = vezes - 1;
-        }
-
-        // se B eh negativo, trocamos o sinal do resultado
-        if(b < 0){
-            acumula = 0 - acumula;
-        }
-        
-        return acumula;
-    }
 }
---
+++ codigoTF.asm
@@ -0,0 +1,103 @@
+# João Vitor Vogel
+# joao.vogel@edu.pucrs.br
+# 23/11/2023
+
+.text
+	.globl	main		# onde o código inicia.
+	
+main:
+    xor	$t0,$t0,$t0 	# zera o $t0 = (i)
+    xor	$t1,$t1,$t1	    # zera o $t1 = variavel vetor A
+    xor	$t2,$t2,$t2    	# zera o $t2 = variavel vetor B
+    xor	$t3,$t3,$t3	    # zera o $t3 = resultado de operação
+    xor	$t4,$t4,$t4	    # zera o $t4 = endereço vetor A
+    xor	$t5,$t5,$t5	    # zera o $t5 = endereço vetor B
+    xor	$t6,$t6,$t6     # zera o $t6 = endereço vetor C
+
+    la  $t4, a          # $t4 recebe o endereço de A(i)
+    la  $t5, b          # $t5 recebe o endereço de B(i)
+    la  $t6, c          # $t6 recebe o endereço de C(i)
+soma:
+    lw  $t1, 0($t4)     # $t1 recebe o valor de A(i)
+    lw  $t2, 0($t5)     # $t2 recebe o valor de B(i)
+
+    add $t3, $t1, $t2   # $t3 recebe ($t1 + $t2)
+    sw  $t3, 0($t6)     # guarda $t3 na memoria C(i)
+
+    add	$t0,$t0,1	    # incrementa o (i)
+    add	$t4,$t4,4	    # incrementa o endereço de A (+4 porque é uma word)
+	add	$t5,$t5,4	    # incrementa o endereço de B (+4 porque é uma word)
+    add	$t6,$t6,4	    # incrementa o endereço de D (+4 porque é uma word)
+
+    blt	$t0, 10, soma	# pula para a "soma" se i < n ($t0 < 10)
+
+
+    xor	$t0,$t0,$t0 	# zera o $t0 = (i)
+    xor	$t1,$t1,$t1	    # zera o $t1 = variavel vetor A
+    xor	$t2,$t2,$t2    	# zera o $t2 = variavel vetor B
+    xor	$t3,$t3,$t3	    # zera o $t3 = resultado de operação
+    xor	$t4,$t4,$t4	    # zera o $t4 = endereço vetor A
+    xor	$t5,$t5,$t5	    # zera o $t5 = endereço vetor B
+    xor	$t6,$t6,$t6     # zera o $t6 = endereço vetor D
+
+    la  $t4, a          # $t4 recebe o endereço de A(i)
+    la  $t5, b          # $t5 recebe o endereço de B(i)
+    la  $t6, d          # $t6 recebe o endereço de D(i)
+subtrai:
+    lw  $t1, 0($t4)     # $t1 recebe o valor de A(i)
+    lw  $t2, 0($t5)     # $t2 recebe o valor de B(i)
+
+    sub $t3, $t1, $t2   # $t3 recebe ($t1 - $t2)
+    sw  $t3, 0($t6)     # guarda $t3 na memoria D(i)
+
+    add	$t0,$t0,1	    # incrementa o (i)
+    add	$t4,$t4,4	    # incrementa o endereço de A (+4 porque é uma word)
+	add	$t5,$t5,4	    # incrementa o endereço de B (+4 porque é uma word)
+    add	$t6,$t6,4	    # incrementa o endereço de D (+4 porque é uma word)
+
+    blt	$t0, 10, subtrai # pula para o "subtrai" se i < n ($t0 < 10)
+
+
+    xor	$t0,$t0,$t0 	# zera o $t0 = (i)
+    xor	$t1,$t1,$t1	    # zera o $t1 = variavel vetor C
+    xor	$t2,$t2,$t2    	# zera o $t2 = variavel vetor D
+    xor	$t3,$t3,$t3	    # zera o $t3 = resultado de operação
+    xor	$t4,$t4,$t4	    # zera o $t4 = endereço vetor C
+    xor	$t5,$t5,$t5	    # zera o $t5 = endereço vetor D
+    xor	$t6,$t6,$t6	    # zera o $t6 = endereço de mv
+
+    la  $t4, c          # $t4 recebe o endereço de C(i)
+    la  $t5, d          # $t5 recebe o endereço de D(i)
+maior:
+    lw  $t1, 0($t4)     # $t1 recebe o valor de C(i)
+    lw  $t2, 0($t5)     # $t2 recebe o valor de D(i)
+
+    blt	$t1, $t3, proximoMaiorC # pula para o "proximoMaiorC" se $t1 < $t3 (c < mv)
+    add $t3, $t1, 0
+
+proximoMaiorC:
+    blt	$t2, $t3, proximoMaiorD # pula para o "proximoMaiorD" se $t2 < $t3 (d < mv)
+    add $t3, $t1, 0
+
+proximoMaiorD:
+    add	$t0,$t0,1	    # incrementa o (i)
+    add	$t4,$t4,4	    # incrementa o endereço de A (+4 porque é uma word)
+	add	$t5,$t5,4	    # incrementa o endereço de B (+4 porque é uma word)
+
+    blt	$t0, 10, maior  # pula para o "maior" se i < n ($t0 < 10)
+
+    la  $t6, mv         # $t6 recebe o endereço de mv
+    sw  $t3, 0($t6)     # guarda $t3 na memoria mv
+
+
+end:	j	end
+
+
+.data
+a:	.word	7 15 9 22 5 -3 -6 -17 -1 -21	# Vetor A
+b:	.word	16 13 5 9 54 -4 -5 -8 -2 -12	# Vetor B
+c:	.word	0 0 0 0 0 0 0 0 0 0				# Vetor C
+d:	.word	0 0 0 0 0 0 0 0 0 0				# Vetor D
+mv:	.word	0								# Maior valor
+st:	.word	0								# Resultado soma
+sm:	.word	0								# Resultado final
